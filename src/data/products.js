064e8a0cc7f86a2db9f7a2af48a73f307409b3ba
ramiporta <p0rt4csd@gmail.com>
2023-05-23 17:19:46 -0300

    data de prueba

---
+++ src/data/products.js
@@ -0,0 +1,91 @@
+export const PRODUCTS = [
+  {
+    id: 1,
+    category: 1,
+    name: 'Remera lisa',
+    price: 5000,
+    description: 'Remera negra lisa corte oversize, la que no puede faltarte',
+    image: 'https://http2.mlstatic.com/D_NQ_NP_812629-MLA50540346177_072022-W.jpg',
+  },
+  {
+    id: 2,
+    category: 1,
+    name: 'Remera estampada',
+    price: 6000,
+    description: 'Remera de algodón con estampa en serigrafia premium',
+    image:
+      'https://www.warriorsdeportes.com/wp-content/uploads/2019/10/1657382494806cf668e1fbc8d3086b1acceac42868-768x768.jpg',
+  },
+  {
+    id: 3,
+    category: 2,
+    name: 'Jean slim-fit',
+    price: 13000,
+    description: 'Jean clásico slim-fit para toda ocasion',
+    image:
+      'https://resources.claroshop.com/medios-plazavip/fotos/productos_sears1/original/2957845.jpg',
+  },
+  {
+    id: 4,
+    category: 2,
+    name: 'Pantalón carpintero',
+    price: 1.5,
+    description: 'Pantalón de bull rígido carpintero para uso diario con mucho estilo',
+    image:
+      'https://onboardsk8.com/wp-content/uploads/2019/12/Pantalon-Simple-Co-Jean-Carpintero-denim-Black-2.jpg',
+  },
+  {
+    id: 5,
+    category: 3,
+    name: 'Air force 1 low undefeated 5 on it',
+    price: 73000,
+    description:
+      'Nike air force 1 vs dunk low undefeated 5 on it son las mejores zapatillas del mundo',
+    image: 'https://nextgrail.dk/wp-content/uploads/2021/08/templatesneakers-11-900x900.jpg',
+  },
+  {
+    id: 6,
+    category: 3,
+    name: 'SB Dunk yellow bear x Grateful dead',
+    price: 137000,
+    description: 'Pan de Yema de trigo de masa madre',
+    image:
+      'https://www.kicksonfire.com/wp-content/uploads/2020/06/Grateful-Dead-x-Nike-SB-Dunk-Low-Yellow-Bear-3.jpeg?x27993',
+  },
+  {
+    id: 7,
+    category: 4,
+    name: 'Hoodie oversize chocolate',
+    price: 13500,
+    description: 'Buzo de algodón frisado premium color chocolate. La tendencia de este invierno',
+    image:
+      'https://studios-tc.com/wp-content/uploads/2021/10/oversize-hoodie-chocolate-front-1587x1800.jpg',
+  },
+  {
+    id: 8,
+    category: 4,
+    name: 'Hoodie tussy pact',
+    price: 19700,
+    description: 'Zarpado buzo de la marca tussy. Toda la onda',
+    image:
+      'https://d3ugyf2ht6aenh.cloudfront.net/stores/001/021/090/products/buzo-aa24-back1-2cf103542961144cd016063344706529-480-0.png',
+  },
+  {
+    id: 9,
+    category: 5,
+    name: 'Gorra Nike SB',
+    price: 7900,
+    description: 'La gorra de los skaters',
+    image:
+      'https://http2.mlstatic.com/gorra-trucker-nike-sb-aero-cap-verde-olivo-para-hombre-D_NQ_NP_674993-MLM27396064328_052018-O.jpg',
+  },
+  {
+    id: 10,
+    category: 5,
+    name: 'Mini Bag Louis Vuitton',
+    price: 793000,
+    description: 'Mini bag cheto mal. Para los que tienen toda la teka',
+    image:
+      'https://images.summitmedia-digital.com/esquiremagph/images/2019/07/31/Small-Bags-Trend-01.jpg',
+  },
+];
