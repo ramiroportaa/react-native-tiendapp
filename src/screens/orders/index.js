833e6c27d10f83c82a019a9420e012e382864532
ramiporta <p0rt4csd@gmail.com>
2023-06-01 17:45:34 -0300

    CRUD de ordenes listo

--- src/screens/orders/index.js
+++ src/screens/orders/index.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import { useEffect } from 'react';
 import { View, FlatList } from 'react-native';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { styles } from './styles';
 import { OrderItem } from '../../components';
-import { ORDERS } from '../../data/orders';
+import { deleteOrderById, getOrders } from '../../store/actions';
 
 const Orders = () => {
+  const dispatch = useDispatch();
+  const orders = useSelector((state) => state.orders.data);
+
+  useEffect(() => {
+    dispatch(getOrders());
+  }, []);
+
   const keyExtractor = (item) => item.id.toString();
   const onRemove = (id) => {
-    console.warn(id);
+    dispatch(deleteOrderById(id));
   };
   const renderItem = ({ item }) => <OrderItem item={item} onRemove={onRemove} />;
   return (
     <View style={styles.container}>
-      <FlatList data={ORDERS} keyExtractor={keyExtractor} renderItem={renderItem} />
+      <FlatList data={orders} keyExtractor={keyExtractor} renderItem={renderItem} />
     </View>
   );
 };
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,13 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
-import { cartReducer, categoriesReducer, productsReducer } from './reducers';
+import { cartReducer, categoriesReducer, orderReducer, productsReducer } from './reducers';
 
 const rootReducers = combineReducers({
   categories: categoriesReducer,
   products: productsReducer,
   cart: cartReducer,
+  orders: orderReducer,
 });
 
 export default createStore(rootReducers, applyMiddleware(thunk));
